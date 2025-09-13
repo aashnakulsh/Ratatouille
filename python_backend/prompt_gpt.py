66f16ada30294d5749fbd1de070687a9657dd0d8
mzaher25 <157839408+mzaher25@users.noreply.github.com>
2025-09-13 13:13:23 -0400

    Update parse_resp

--- python_backend/prompt_gpt.py
+++ python_backend/prompt_gpt.py
@@ -26,26 +26,29 @@ def prompt_gpt(prompt, image_path):
 
 def parse_response(response):
     resp = {}
-    resp_list = response.split("\n")
     
-    if len(resp_list) != 3: raise ValueError("wrong!")
+    try: 
+        resp_list = response.split("\n")
+        if len(resp_list) != 3: raise ValueError("wrong!")
 
-    #integer
-    rating = resp_list[0]
-    digit = "".join(filter(str.isdigit, rating))
+        #integer
+        rating = resp_list[0]
+        digit = "".join(filter(str.isdigit, rating))
 
-    if len(digit)<1: raise ValueError("wrong!")
+        if len(digit)<1: raise ValueError("wrong!")
 
-    digit = int(digit)
-    if digit > 5 or digit <1: raise ValueError("wrong rating!")
+        digit = int(digit)
+        if digit > 5 or digit <1: raise ValueError("wrong rating!")
 
-    resp["rating"] = digit
+        resp["rating"] = digit
 
-    #feedback:
-    if len(resp_list[1])<1: raise ValueError("wrong!")
-    if len(resp_list[2])<1: raise ValueError("wrong!")
+        #feedback:
+        if len(resp_list[1])<1: raise ValueError("wrong!")
+        if len(resp_list[2])<1: raise ValueError("wrong!")
 
-    resp["pos"] = resp_list[1]
-    resp["impr"] = resp_list[2]
-
+        resp["pos"] = resp_list[1]
+        resp["impr"] = resp_list[2]
+    #if GPT outputs garbage we can output a generic response
+    except:
+        resp = {"rating":3, "pos": "I can see you put in a good effort here", "impr": "I fear not even a rat would eat this"}
     return resp
