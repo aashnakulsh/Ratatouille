9d6538e4172ec96ebc17957e538b71a6407e5659
windvalleys <149912998+windvalleys@users.noreply.github.com>
2025-09-13 10:30:30 -0400

    added post function

--- python_backend/main.py
+++ python_backend/main.py
@@ -1,1 +1,28 @@
+from fastapi import FastAPI, File, Form, UploadFile
+import shutil
+import tempfile
+import recipe_builder
+import prompt_gpt
 
+app = FastAPI()
+
+omlette = recipe_builder.build_omlette()
+pbj = recipe_builder.build_pbj()
+
+@app.post("/process/")
+async def process_file(
+    image: UploadFile = File(...),
+    recipeNum: int = Form(...),
+    step: int = Form(...)
+):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp:
+        shutil.copyfileobj(image.file, tmp)
+        tmp_path = tmp.name
+    if recipeNum == 0:
+        recipe = omlette
+    else:
+        recipe = pbj
+    prompt = recipe.run_step(step)
+    result_text = prompt_gpt.prompt_gpt(prompt, tmp_path)
+
+    return {"result": result_text}
