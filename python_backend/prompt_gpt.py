17d888c1a8eeec94efb1c3dd8039731f220df9e2
mzaher25 <157839408+mzaher25@users.noreply.github.com>
2025-09-13 13:07:21 -0400

    Update prompt_gpt.py

--- python_backend/prompt_gpt.py
+++ python_backend/prompt_gpt.py
@@ -2,7 +2,7 @@ import base64
 import openai
 from openai import OpenAI
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "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"
 
 client = OpenAI(api_key = OPENAI_API_KEY)
 
@@ -25,26 +25,29 @@ def prompt_gpt(prompt, image_path):
 
 def parse_response(response):
     resp = {}
-    resp_list = response.split("\n")
     
-    if len(resp_list) != 3: raise ValueError("wrong!")
+    try: 
+        resp_list = response.split("\n")
+        if len(resp_list) != 3: raise ValueError("wrong!")
 
-    #integer
-    rating = resp_list[0]
-    digit = "".join(filter(str.isdigit, rating))
+        #integer
+        rating = resp_list[0]
+        digit = "".join(filter(str.isdigit, rating))
 
-    if len(digit)<1: raise ValueError("wrong!")
+        if len(digit)<1: raise ValueError("wrong!")
 
-    digit = int(digit)
-    if digit > 5 or digit <1: raise ValueError("wrong rating!")
+        digit = int(digit)
+        if digit > 5 or digit <1: raise ValueError("wrong rating!")
 
-    resp["rating"] = digit
+        resp["rating"] = digit
 
-    #feedback:
-    if len(resp_list[1])<1: raise ValueError("wrong!")
-    if len(resp_list[2])<1: raise ValueError("wrong!")
+        #feedback:
+        if len(resp_list[1])<1: raise ValueError("wrong!")
+        if len(resp_list[2])<1: raise ValueError("wrong!")
 
-    resp["pos"] = resp_list[1]
-    resp["impr"] = resp_list[2]
-
+        resp["pos"] = resp_list[1]
+        resp["impr"] = resp_list[2]
+    #if GPT outputs garbage we can output a generic response
+    except:
+        resp = {"rating":3, "pos": "I can see you put in a good effort here", "impr": "I fear not even a rat would eat this"}
     return resp
